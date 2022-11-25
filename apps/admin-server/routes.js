4abeaf23883d37ffb9102e84ba9c421c93f97b97
Thirunavukkarasu <thirunavukkarasu.m@getvymo.com>
2022-11-25 22:36:17 +0530

    added healthcheck route

--- apps/admin-server/routes.js
+++ apps/admin-server/routes.js
@@ -14,6 +14,19 @@ module.exports = () => {
   router.use("/order", orderRoutes());
   router.use("/rnc", rncRoutes());
   router.use("/settings", settingsRoutes());
+  router.get('/healthcheck', async (_req, res, _next) => {
+    const healthcheck = {
+        uptime: process.uptime(),
+        message: 'OK',
+        timestamp: Date.now()
+    };
+    try {
+        return res.send(healthcheck);
+    } catch (error) {
+        healthcheck.message = error;
+        return res.status(503).send("Server is having issues!");
+    }
+});
 
   return router;
 };
